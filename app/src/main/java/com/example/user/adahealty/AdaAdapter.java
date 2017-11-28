51ec7da30f695d6eed28c016d25b7fd77aaefec9
sam <muhammadusamah76@gmail.com>
2017-11-28 17:30:25 +0700

    Membuat AdaAdapter

---
+++ app/src/main/java/com/example/user/adahealty/AdaAdapter.java
@@ -0,0 +1,32 @@
+package com.example.user.adahealty;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Asus on 11/28/2017.
+ */
+
+public class AdaAdapter extends RecyclerView.Adapter<AdaAdapter.ViewHolder>{
+    @Override
+    public AdaAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(AdaAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
